f467e15eeff7ab0ef71dadca24b243ba05738206
ArunKuriakose15 <arunkp1515@gmail.com>
2025-02-14 21:01:17 +0530

    server

--- backend/controllers/userRouter.js
+++ backend/controllers/userRouter.js
@@ -20,25 +20,63 @@ async function hashPasswordGenerator(password) {
 
 router.post("/signup", async (req, res) => {
     try {
+        if (!req.body || !req.body.email || !req.body.password) {
+            return res.status(400).json({ status: "error", message: "Missing email or password" });
+        }
         const data = req.body
         const userCheck = await userModel.findOne({ email: data.email });
-        
+
         if (userCheck) {
             return res.json({ status: "email exists" });
         }
-        
+
         let password = data.password
         const hashedPassword = await hashPasswordGenerator(password)
         data.password = hashedPassword
         let user = new userModel(data)
-        let result =await user.save()
+        let result = await user.save()
         res.json({
-            status: "success",message: "User registered successfully"
+            status: "success", message: "User registered successfully"
         })
     }
     catch (error) {
-        res.json({ status: "error" })
+        res.json({ status: "error", message: error })
     }
-}
-)
+})
+
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ message: "Invalid email" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid password" });
+        }
+
+        const token = jwt.sign(
+            { userId: user._id, role: user.role }, 
+            process.env.JWT_SECRET,
+            { expiresIn: "1h" }
+        );
+
+        res.status(200).json({ message: "Login successful",user, token });
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+});
+
+
+
+
+
+
+
+
+
+
 module.exports = router
